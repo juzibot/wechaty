3d2cc91938822d8a5e61cf699b2f9f31d1ee974b
NickWang <brotherstyx@gmail.com>
2022-06-29 15:20:43 +0800

    test: 💍 fix from

--- src/user-modules/room.spec.ts
+++ src/user-modules/room.spec.ts
@@ -24,7 +24,7 @@ import {
 }             from 'tstest'
 
 import type * as PUPPET from '@juzi/wechaty-puppet'
-import { PuppetMock }   from 'wechaty-puppet-mock'
+import { PuppetMock }   from '@juzi/wechaty-puppet-mock'
 
 import { WechatyBuilder } from '../wechaty-builder.js'
 import type {
