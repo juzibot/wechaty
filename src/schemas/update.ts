efc9918e68c32ac39fb84ebcbdc3a04b4a7c92f2
NickWang <brotherstyx@gmail.com>
2022-08-02 19:14:07 +0800

    feat: 🎸 info update type

--- src/schemas/update.ts
+++ src/schemas/update.ts
@@ -1,14 +1,29 @@
 /* eslint-disable valid-typeof */
-import type * as PUPPET from '@juzi/wechaty-puppet'
+import * as PUPPET from '@juzi/wechaty-puppet'
 
-export interface InfoUpdateInterface {
-  type: PUPPET.types.Payload,
-  id: string,
-  updates: InfoUpdateValuePair[],
-}
+type ContactImportantKeys = 'name' | 'tags' | 'alias' | 'phone' | 'description'
+type RoomImportantKeys = 'topic' | 'memberIdList' | 'ownerId'
 
-export interface InfoUpdateValuePair {
-  key: string,
-  oldValue: string,
-  newValue: string
-}
+type ContactUpdatableKeys = Omit<PUPPET.payloads.Contact, ContactImportantKeys>
+type RoomUpdateableKeys = Omit<PUPPET.payloads.Room, RoomImportantKeys>
+
+type PayloadTypeToUpdateInterface<U> = {
+  [SubType in keyof U]: {
+    type: SubType,
+    id: string,
+    updates: InfoUpdateValuePair<U[SubType]>[],
+  }
+}[keyof U]
+
+type InfoUpdateValuePair<U> = {
+  [SubType in keyof U]: {
+    key: SubType,
+    oldValue?: U[SubType],
+    newValue?: U[SubType],
+  }
+}[keyof U]
+
+export type InfoUpdateInterface = PayloadTypeToUpdateInterface<{
+  [PUPPET.types.Payload.Contact]: ContactUpdatableKeys,
+  [PUPPET.types.Payload.Room]: RoomUpdateableKeys,
+}>
