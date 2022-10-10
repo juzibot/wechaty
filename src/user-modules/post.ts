77b7f847d7fea8e3747707230327d5b2d0029cfd
NickWang <brotherstyx@gmail.com>
2022-10-10 14:07:10 +0800

    feat: 🎸 redesign post sayable iterator

--- src/user-modules/post.ts
+++ src/user-modules/post.ts
@@ -301,19 +301,19 @@ class PostMixin extends wechatifyMixinBase() {
   async * [Symbol.asyncIterator] (): AsyncIterableIterator<Sayable> {
     log.verbose('Post', '[Symbol.asyncIterator]()')
 
+    const payloadToSayable = payloadToSayableWechaty(this.wechaty)
+
     if (PUPPET.payloads.isPostServer(this.payload)) {
-      for (const messageId of this.payload.sayableList) {
-        const message = await this.wechaty.Message.find({ id: messageId })
-        if (message) {
-          const sayable = await message.toSayable()
-          if (sayable) {
-            yield sayable
-          }
+      for (const sayableId of this.payload.sayableList) {
+        const sayablePayload = await this.wechaty.puppet.postPayloadSayable(this.id!, sayableId)
+        const sayable = await payloadToSayable(sayablePayload)
+
+        if (sayable) {
+          yield sayable
         }
       }
 
     } else {  // client
-      const payloadToSayable = payloadToSayableWechaty(this.wechaty)
       for (const sayablePayload of this.payload.sayableList) {
         const sayable = await payloadToSayable(sayablePayload)
         if (sayable) {
