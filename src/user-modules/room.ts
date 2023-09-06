524e538ebaeed44b4f518586063c34a59382a399
suchang <su_chang@yeah.net>
2023-09-06 15:04:57 +0800

    feat: :sparkles: add room admins methods

--- src/user-modules/room.ts
+++ src/user-modules/room.ts
@@ -1223,6 +1223,16 @@ class RoomMixin extends MixinBase implements SayableSayer {
     return this.wechaty.puppet.roomPermission(this.id, permission)
   }
 
+  async addAdmins (contactList: ContactInterface[]): Promise<void> {
+    log.verbose('Room', 'addAdmins(%s)', contactList)
+    await this.wechaty.puppet.roomAddAdmins(this.id, contactList.map(c => c.id))
+  }
+
+  async delAdmins (contactList: ContactInterface[]): Promise<void> {
+    log.verbose('Room', 'delAdmins(%s)', contactList)
+    await this.wechaty.puppet.roomDelAdmins(this.id, contactList.map(c => c.id))
+  }
+
   async transfer (contact: ContactInterface): Promise<void> {
     if (!(await this.has(contact))) {
       throw new Error('cannot transfer room owner to a contact that\'s not in room.')
